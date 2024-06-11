222a01b286875292190a9354dc4243b4a677bab0
Greg Doud <gpdoud@gmail.com>
2024-06-11 14:52:07 -0400

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MyFirstConsoleApp;
+
+/*
+ * This is my first class
+ */
+
+/// <summary>
+/// This is a cool class!
+/// </summary>
+internal class Program {
+
+    static int i = 0;
+    static int? j = null;
+
+    // Property
+    public static string Name { get; set; } = "Greg"; // property
+
+    // Method
+    static void Main(string[] args) { // method
+
+        Program pgm = new Program();
+
+        Console.WriteLine($"Hello, {Name} World!");
+    }
+
+    static void Print(string message) { 
+        Console.WriteLine(message);
+    }
+
+}
+
